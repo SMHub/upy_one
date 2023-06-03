895a3882d60310ea43313c2a531c3ae0361b5348
SM <smehbub@outlook.com>
2023-06-02 22:41:31 -0400

    prv to pub

--- boot.py
+++ boot.py
@@ -7,9 +7,6 @@ import time
 
 print('hello ugit users!')
 
-print('I am working on an issue with ugit running into mem errors when connected to a computer')
-print('ugit seems to work well when battery powered or not in an active REPL session.')
-
 print('I hope you enjoy it!')
     
 
